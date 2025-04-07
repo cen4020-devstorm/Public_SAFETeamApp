a05bd96c37f8135172d200f5faff6e324d68cfef
AlizeeFR <robinsonfaitha+github@gmail.com>
2025-04-07 19:54:11 -0400

    testing migrations

--- db/migrate/20250407213209_add_number_of_passengers_to_ride_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberOfPassengersToRideRequests < ActiveRecord::Migration[7.1]
-  def change
-    add_column :ride_requests, :number_of_passengers, :integer, default: 1, null: false
-  end
-end
