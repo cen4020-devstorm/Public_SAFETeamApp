b29f649c74451eafab9d741ca198d6160a573d9d
rosegab1 <gmarierose11@gmail.com>
2025-04-07 13:11:25 -0400

    Add files via upload
    Pages controller that will direct the user to the login page within operating hours or direct them to a message if outside of operating hours.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+    def ride_redirect
+        now = Time.current.in_time_zone("Eastern Time (US & Canada)")
+
+        if within_op_hours(now)
+        redirect_to login_path
+        else
+        redirect_to outside_hours_path
+        end
+    end
+
+    private
+
+    def within_op_hours(time)
+    allowed_days = [0, 1, 2, 3, 4]
+    current_day = time.wday
+    current_time = time.strftime("%H:%M")
+
+    if allowed_days.include?(current_day)
+        return true if current_time > "18:30" || current_time < "02:00"
+    end
+    if current_day == 5 && current_time < "02:00"
+        return true
+    end
+
+    return false
+    end
+end
